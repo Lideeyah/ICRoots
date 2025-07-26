d9901ba4a54876a933ac81651f41b109bc9131da
Oluwalana Ajayi <ajayioluwalana@gmail.com>
2025-07-26 16:21:58 +0100

    chore(loans): update Cargo.toml and reformat imports in lib.rs

--- src/backend/canisters/loans/src/lib.rs
+++ src/backend/canisters/loans/src/lib.rs
@@ -1,9 +1,9 @@
-- use ic_cdk::api::stable;
-- use ic_cdk::{export::Principal, storage};
-+ use ic_cdk::stable::{stable64_read, stable64_write, stable64_size};
-+ use candid::{CandidType, Deserialize, Principal};
-+ use std::cell::RefCell;
-+ use std::time::{SystemTime, UNIX_EPOCH};
+use ic_cdk::api::stable;
+use ic_cdk::{export::Principal, storage};
+use ic_cdk::stable::{stable64_read, stable64_write, stable64_size};
+use candid::{CandidType, Deserialize, Principal};
+use std::cell::RefCell;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 
 /* ---------- DATA TYPES ---------- */
